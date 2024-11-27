c8993ee11791f55f45abe67cf0683f1f128852e9
Ju-seph <crecendia2009@outlook.com>
2024-11-27 17:13:39 -0500

    User update

--- models/user.py
+++ models/user.py
@@ -2,20 +2,23 @@ from datetime import datetime
 
 class User:
 
-    def __init__(self, cedula, nombre, usuario, clave, rol):
-        self.cedula= cedula
-        self.nombre= nombre
-        self.usuario= usuario
-        self.clave= clave
+    def __init__(self, ced, name,lastName, username,password , rol, direction,status):
+        self.ced= ced
+        self.name= name
+        self.lastName= lastName
+        self.username= username
+        self.password= password
         self.rol= rol
+        self.direction= direction
+        self.status= status
 
-    def obtener_user(self):
+    def getUser(self):
         return self.__dict__
     
 
-    def crear_usuario(self):
-        self.fecha_creacion = datetime.now()
+    def createUser(self):
+        self.createDateTime = datetime.now()
 
-    def update_usuario(self):
-        self.fecha_modificacion = datetime.now()
+    def updateUser(self):
+        self.updateDateTime = datetime.now()
     
