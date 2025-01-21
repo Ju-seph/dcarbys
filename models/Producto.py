8e3867f5c96973a0b634f139d3fb489c5d5b967e
Ju-seph <crecendia2009@outlook.com>
2025-01-21 15:58:21 -0500

    principal

--- models/Producto.py
+++ models/Producto.py
@@ -5,20 +5,37 @@ class Producto:
     def __init__(self, nombreProducto, precio, cantidad, status, categoria=None, descripcion=None, 
                  imagen_url=None, tiempo_preparacion=None, destacado=False):
         self.nombreProducto = nombreProducto
-        self.precio = precio
-        self.cantidad = cantidad
+        self.precio = round(float(precio), 2)  # Asegurar precio en formato decimal y con 2 decimales
+        self.cantidad = int(cantidad)  # Asegurar que la cantidad sea un entero
         self.status = status
         self.categoria = categoria
         self.descripcion = descripcion
         self.imagen_url = imagen_url
         self.tiempo_preparacion = tiempo_preparacion
         self.destacado = destacado
+        self.createDateTime = None  # Se inicializa vacío
+        self.updateDateTime = None  # Se inicializa vacío
 
     def getProducto(self):
+        """
+        Retorna un diccionario del producto para ser guardado en la base de datos.
+        """
         return self.__dict__
 
     def createProducto(self):
+        """
+        Establece la fecha de creación del producto.
+        """
         self.createDateTime = datetime.now()
 
     def updateProducto(self):
+        """
+        Establece la fecha de última actualización del producto.
+        """
         self.updateDateTime = datetime.now()
+
+    def setImagePath(self, image_filename):
+        """
+        Establece la ruta de la imagen para el producto.
+        """
+        self.imagen_url = f"/img/{image_filename}"
