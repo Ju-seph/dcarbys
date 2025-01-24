81bece0df74f7091d0ea7fcb2e40e2f56ac14ebe
Ju-seph <crecendia2009@outlook.com>
2025-01-24 18:15:44 -0500

    probando el edit
    ctl product tiene error revisar

--- controllers/ctl_productos.py
+++ controllers/ctl_productos.py
@@ -4,6 +4,7 @@ import time
 from database.mongodb import Mongodb
 from bson.objectid import ObjectId
 from models.Producto import Producto
+from bson.errors import InvalidId
 import os
 
 # Configuración de la carpeta de subida
@@ -25,16 +26,18 @@ def ver_productos(request):
             lista_productos = list(productos)
 
             for producto in lista_productos:
-                producto["_id"] = str(producto["_id"])
+                producto["_id"] = str(producto["_id"])  # Convierte ObjectId en cadena
+                producto["id"] = producto["_id"]        # Agrega un alias "id"
 
-            datos = {"data": lista_productos}
-            return json.dumps(datos, default=str), 200
+            datos = {"data": lista_productos}  # Estructura esperada por DataTables
+            return jsonify(datos), 200  # Usa jsonify directamente para JSON válido
 
         except Exception as e:
             print(f"Error al obtener los productos: {e}")
             return jsonify({"message": f"Error al obtener los productos: {e}"}), 500
 
     return jsonify({"message": "Petición Incorrecta"}), 405
+
 
 
 def save_product(request):
@@ -110,6 +113,9 @@ def save_product(request):
 
 
 
+
+
+
 def edit_product(request):
     # Inicializar el diccionario de respuesta
     response = {"status": "error", "message": "", "data": None}
@@ -133,6 +139,13 @@ def edit_product(request):
                 response["message"] = "ID del producto, nombre, precio y cantidad son obligatorios."
                 return jsonify(response), 400
 
+            # Validar y convertir el ObjectId
+            try:
+                producto_id = ObjectId(producto_id)
+            except InvalidId:
+                response["message"] = "El ID proporcionado no es válido."
+                return jsonify(response), 400
+
             # Validar que el precio sea decimal y la cantidad un entero
             try:
                 precio = float(precio)
@@ -142,14 +155,97 @@ def edit_product(request):
                 return jsonify(response), 400
 
             # Buscar el producto en la base de datos
-            producto_existente = db.productos.find_one({"_id": ObjectId(producto_id)})
+            producto_existente = db.productos.find_one({"_id": producto_id})
             if not producto_existente:
                 response["message"] = "El producto no existe."
                 return jsonify(response), 404
 
+            
+
+            # Crear objeto Producto con los datos actualizados
+            producto = Producto(
+                nombreProducto=nombreProducto,
+                precio=precio,
+                cantidad=cantidad,
+                status="activo" if not status else status,
+                categoria=categoria,
+                descripcion=descripcion,
+                imagen_path=imagen_path,
+                tiempo_preparacion=tiempo_preparacion,
+                destacado=destacado
+            )
+
+            # Actualizar el producto en la base de datos
+            db.productos.update_one(
+                {"_id": producto_id},
+                {"$set": producto.getProducto()}
+            )
+
+            # Producto actualizado exitosamente
+            response["status"] = "success"
+            response["message"] = "Producto actualizado correctamente."
+            return jsonify(response), 200
+
+        except Exception as e:
+            # Manejar errores y enviar mensaje de error
+            print(f"Error al actualizar el producto: {e}")
+            response["message"] = f"Ocurrió un error al actualizar el producto: {e}"
+            return jsonify(response), 500
+
+    # Si el método no es POST, devolver error 405
+    response["message"] = "Método no permitido."
+    return jsonify(response), 405
+
+
+def edit_product(request):
+    # Inicializar el diccionario de respuesta
+    response = {"status": "error", "message": "", "data": None}
+
+    # Verificar si el método de la solicitud es POST
+    if request.method == 'POST':
+        try:
+            # Obtener datos del formulario
+            nombreProducto = request.form.get("u_nombreProducto")
+            precio = request.form.get("u_precio")
+            cantidad = request.form.get("u_cantidad")
+            status = request.form.get("u_status")
+            categoria = request.form.get("u_categoria")
+            descripcion = request.form.get("u_descripcion")
+            imagen_path = request.form.get("u_imagen_path")
+            tiempo_preparacion = request.form.get("u_tiempo_preparacion")
+            destacado = request.form.get("u_destacado") == 'true'
+
+            # Validar que los campos requeridos no estén vacíos
+            if not (nombreProducto and precio and cantidad):
+                response["message"] = "Nombre del producto, precio y cantidad son obligatorios."
+                return jsonify(response), 400
+
+            # Validar que el precio sea decimal y la cantidad un entero
+            try:
+                precio = float(precio)
+                cantidad = int(cantidad)
+            except ValueError:
+                response["message"] = "El precio debe ser un número decimal y la cantidad un número entero."
+                return jsonify(response), 400
+
+            # Verificar si el producto ya existe en la base de datos
+            existe = db.productos.find_one({"nombreProducto": nombreProducto})
+            if existe:
+                 producto = Producto(
+                nombreProducto=nombreProducto,
+                precio=precio,
+                cantidad=cantidad,
+                status="activo" if not status else status,
+                categoria=categoria,
+                descripcion=descripcion,
+                imagen_path=imagen_path,
+                tiempo_preparacion=tiempo_preparacion,
+                destacado=destacado
+            )
+
             # Manejar la carga de una nueva imagen
             imagen_file = request.files.get("u_imagen_producto")
-            imagen_path = producto_existente.get("imagen_path")  # Mantener la imagen actual si no se sube una nueva
+            imagen_path = existe.get("imagen_path")  # Mantener la imagen actual si no se sube una nueva
 
             if imagen_file and allowed_file(imagen_file.filename):
                 # Generar un nombre de archivo seguro
@@ -167,32 +263,20 @@ def edit_product(request):
                 imagen_path = f"/img/{filename}"  # Actualizar la ruta relativa
 
                 # Eliminar la imagen anterior si existe
-                if "imagen_path" in producto_existente and os.path.exists(os.path.join(UPLOAD_FOLDER, producto_existente["imagen_path"].split('/')[-1])):
-                    os.remove(os.path.join(UPLOAD_FOLDER, producto_existente["imagen_path"].split('/')[-1]))
+                if "imagen_path" in existe and os.path.exists(
+                    os.path.join(UPLOAD_FOLDER, existe["imagen_path"].split('/')[-1])
+                ):
+                    os.remove(os.path.join(UPLOAD_FOLDER, existe["imagen_path"].split('/')[-1]))
 
-            # Crear objeto Producto con los datos actualizados
-            producto = Producto(
-                nombreProducto=nombreProducto,
-                precio=precio,
-                cantidad=cantidad,
-                status="activo" if not status else status,
-                categoria=categoria,
-                descripcion=descripcion,
-                imagen_path=imagen_path,
-                tiempo_preparacion=tiempo_preparacion,
-                destacado=destacado
-            )
-
-            # Actualizar el producto en la base de datos
-            db.productos.update_one(
-                {"_id": ObjectId(producto_id)},
-                {"$set": producto.getProducto()}
-            )
+                 # Actualizar el producto en la base de datos
+            producto.updateProducto()
+            db.productos.update_one({"_id":ObjectId(existe["_id"])},
+                                    {"$set":producto.getProducto()})
 
             # Producto actualizado exitosamente
             response["status"] = "success"
             response["message"] = "Producto actualizado correctamente."
-            return jsonify(response), 200
+            return jsonify(response), 201
 
         except Exception as e:
             # Manejar errores y enviar mensaje de error
--- public/js/productos.js
+++ public/js/productos.js
@@ -6,49 +6,48 @@ $(document).ready(function () {
         },
         "columns": [
             { "data": "id", "className": "text-center" }, // ID (oculto)
-            { "data": "nombreProducto", "className": "text-center" }, // Nombre del Producto
-            { "data": "precio", "className": "text-center" }, // Precio
-            { "data": "cantidad", "className": "text-center" }, // Cantidad
-            { "data": "categoria", "className": "text-center" }, // Categoría
-            { "data": "descripcion", "className": "text-center" }, // Descripción
+            { "data": "nombreProducto", "className": "text-center" },
+            { "data": "precio", "className": "text-center" },
+            { "data": "cantidad", "className": "text-center" },
+            { "data": "categoria", "className": "text-center" },
+            { "data": "descripcion", "className": "text-center" },
             {
-                "data": "imagen_path", // Imagen
+                "data": "imagen_path",
                 "className": "text-center",
                 "render": function (data) {
-                    if (data) {
-                        return `<img src="${data}" alt="Imagen Producto" style="width: 60px; height: 60px; object-fit: cover;">`;
-                    } else {
-                        return "No disponible";
-                    }
+                    return data
+                        ? `<img src="${data}" alt="Imagen Producto" style="width: 60px; height: 60px; object-fit: cover;">`
+                        : "No disponible";
                 }
             },
-            { "data": "tiempo_preparacion", "className": "text-center" }, // Tiempo de Preparación
+            { "data": "tiempo_preparacion", "className": "text-center" },
             {
-                "data": "destacado", // Destacado
+                "data": "destacado",
                 "className": "text-center",
                 "render": function (data) {
                     return data ? '<i class="bi bi-star-fill text-warning"></i>' : '<i class="bi bi-star"></i>';
                 }
             },
             {
-                "data": null, // Acciones
+                "data": null,
                 "className": "text-center",
-                "defaultContent": '<button type="button" class="btn btn-primary btn-editar-producto"><i class="bi bi-pencil"></i></button> <button type="button" class="btn btn-danger btn-eliminar-producto"><i class="bi bi-trash"></i></button>'
+                "defaultContent": `
+                    <button type="button" class="btn btn-primary btn-editar-producto">
+                        <i class="bi bi-pencil"></i>
+                    </button>
+                    <button type="button" class="btn btn-danger btn-eliminar-producto">
+                        <i class="bi bi-trash"></i>
+                    </button>`
             }
         ],
         "columnDefs": [
             {
-                "targets": 0, // Ocultar la columna ID
+                "targets": 0,
                 "visible": false,
                 "searchable": false
-            },
-            {
-                "targets": -1, // Acciones
-                "data": null,
-                "defaultContent": '<button type="button" class="btn btn-primary btn-editar-producto"><i class="bi bi-pencil"></i></button> <button type="button" class="btn btn-danger btn-eliminar-producto"><i class="bi bi-trash"></i></button>'
             }
         ],
-        "order": [[0, "asc"]], // Ordenar por ID
+        "order": [[0, "asc"]],
         "language": { "url": "https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json" },
         "pageLength": 30,
         "lengthMenu": [10, 20, 30]
@@ -58,18 +57,16 @@ $(document).ready(function () {
     $('#table_producto tbody').on("click", ".btn-eliminar-producto", function () {
         var datos = tabla.row($(this).parents("tr")).data();
 
-        var pregunta = confirm("¿Estás seguro de eliminar el producto " + datos.nombre + "?");
-
-        if (pregunta) {
+        if (confirm(`¿Estás seguro de eliminar el producto "${datos.nombreProducto}"?`)) {
             $.ajax({
                 url: "/del_productos",
                 type: "POST",
                 data: { u_id: datos.id }
             }).done(function () {
-                alert("El producto " + datos.nombre + " fue eliminado correctamente");
-                $("#table_producto").DataTable().ajax.reload();
+                alert(`El producto "${datos.nombreProducto}" fue eliminado correctamente`);
+                tabla.ajax.reload();
             }).fail(function (e) {
-                alert("Error: " + e.responseJSON.message);
+                alert(`Error: ${e.responseJSON.message}`);
             });
         }
     });
@@ -80,56 +77,62 @@ $(document).ready(function () {
 
         $("#productoModal").modal("show");
 
-        var form = document.getElementById("form_producto");
+        // Llenar campos del modal con datos del producto
+        $("#u_nombreProducto").val(datos.nombreProducto);
+        $("#u_precio").val(datos.precio);
+        $("#u_cantidad").val(datos.cantidad);
+        $("#u_categoria").val(datos.categoria);
+        $("#u_descripcion").val(datos.descripcion);
+        $("#u_tiempo_preparacion").val(datos.tiempo_preparacion);
+        $("#u_destacado").prop("checked", datos.destacado);
 
-        document.getElementById("btn-editar-producto").style = "display:visible";
-        document.getElementById("btn-guardar-producto").style = "display:none";
+        // Asegurarse de que el ID del producto se incluya
+        // Si ya existe, actualízalo; si no, créalo
+        if ($("#u_id").length === 0) {
+            $("#form_producto").append(`<input type="hidden" id="u_id" name="u_id" value="${datos.id}">`);
+        } else {
+            $("#u_id").val(datos.id);
+        }
 
-        document.getElementById("u_nombreProducto").value = datos.nombreProducto;
-        document.getElementById("u_precio").value = datos.precio;
-        document.getElementById("u_cantidad").value = datos.cantidad;
-        document.getElementById("u_categoria").value = datos.categoria;
-        document.getElementById("u_descripcion").value = datos.descripcion;
-        document.getElementById("u_tiempo_preparacion").value = datos.tiempo_preparacion;
-        document.getElementById("u_destacado").checked = datos.destacado;
+        // Configurar el formulario para enviar al endpoint de edición
+        $("#form_producto").attr("action", "/edit_productos");
 
-        form.setAttribute("url", "/edit_productos");
+        // Cambiar botones
+        $("#btn-guardar-producto").hide();
+        $("#btn-editar-producto").show();
     });
 
-    // Guardar producto
+    // Guardar producto (crear o editar)
     $('#form_producto').submit(function (e) {
         e.preventDefault();
 
-        var form = $("#form_producto")[0];
+        var form = $(this);
+        var url = form.attr("action");
 
         $.ajax({
-            url: form.getAttribute("url"),
+            url: url,
             type: 'POST',
-            data: new FormData(form),
+            data: new FormData(this),
             processData: false,
             contentType: false,
             cache: false
         }).done(function (e) {
-            if (e.message !== "Producto actualizado correctamente") {
-                alert("Producto creado correctamente");
-            } else {
-                alert("Producto actualizado correctamente");
-            }
+            alert(e.message);
             $("#productoModal").modal("hide");
-            $("#table_producto").DataTable().ajax.reload();
+            tabla.ajax.reload();
         }).fail(function (e) {
-            alert("Error: " + e.responseJSON.message);
+            alert(`Error: Ocurrió un error al actualizar el producto: ${e.responseJSON.message}`);
         });
     });
 
     // Modal para agregar nuevo producto
-    $("#productoModal").on("show.bs.modal", function (event) {
-        var form = document.getElementById("form_producto");
-        form.reset();
+    $("#productoModal").on("show.bs.modal", function () {
+        $("#form_producto")[0].reset();
+        $("#u_id").remove(); // Eliminar campo ID si existe
 
-        document.getElementById("btn-guardar-producto").style = "display:visible";
-        document.getElementById("btn-editar-producto").style = "display:none";
-
-        form.setAttribute("url", "/save_productos");
+        // Configurar para creación
+        $("#form_producto").attr("action", "/save_productos");
+        $("#btn-guardar-producto").show();
+        $("#btn-editar-producto").hide();
     });
 });
